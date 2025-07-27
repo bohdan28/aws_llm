88ed3ec3af35f0962e7f97e862292012d6d791f5
Bohdan Rublevskyi <rublevskyibohdan@gmail.com>
2025-07-27 15:41:40 +0000

    allow ports for llm connection

--- terraform/modules/alb/main.tf
+++ terraform/modules/alb/main.tf
@@ -4,8 +4,8 @@ resource "aws_security_group" "alb" {
   vpc_id      = var.vpc_id
 
   ingress {
-    from_port   = 80
-    to_port     = 80
+    from_port   = 11434
+    to_port     = 11434
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
